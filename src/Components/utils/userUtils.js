8779950b245076068d5e648841bec43cf902620f
KUMARA K.H.A.N.N. <142929083+nngeek195@users.noreply.github.com>
2025-09-03 13:50:44 +0530

    Update userUtils.js

--- src/Components/utils/userUtils.js
+++ src/Components/utils/userUtils.js
@@ -217,7 +217,7 @@ export const validateWithServer = async () => {
     }
 
     try {
-        const response = await fetch('https://worthy-enhancements-pound-around.trycloudflare.com/?utm_source=chatgpt.com/login', {
+        const response = await fetch('https://worthy-enhancements-pound-around.trycloudflare.com/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
