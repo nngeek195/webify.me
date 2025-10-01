8d63240f04a1a7f088e84b468d514f7d5399e56b
Niranga Nayanajith <142929083+nngeek195@users.noreply.github.com>
2025-10-01 22:06:02 +0530

    Update ProfilePictureModal.js

--- src/Components/User/ProfilePictureModal/ProfilePictureModal.js
+++ src/Components/User/ProfilePictureModal/ProfilePictureModal.js
@@ -20,7 +20,7 @@ class ProfilePictureModal extends Component {
 
     fetchPictureOptions = async () => {
         try {
-            const response = await fetch('https://gui-fixes-sale-suppose.trycloudflare.com/profilePictureOptions/6');
+            const response = await fetch('https://dirt-driven-performing-supply.trycloudflare.com/profilePictureOptions/6');
             const data = await response.json();
 
             if (data.success && data.data) {
